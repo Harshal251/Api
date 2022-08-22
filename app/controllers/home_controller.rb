ac242d885707447a1964d3045a78fefd2cf0388c
HARSHAL <109805727+Harshal251@users.noreply.github.com>
2022-08-22 16:30:15 +0530

    Add files via upload

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,34 @@
+class HomeController < ApplicationController
+  def home
+    if current_user
+      show_info({ "message" => "#{current_user.email}! Welcome to the Student Notices API", "user" => current_user_data })
+    else
+      show_info({ "message" => "Welcome to the Student Notices API", "prompt" => "Please Login to perform api actions." })
+    end
+  end
+
+  private
+
+  # Creating user information in json
+
+  def current_user_data
+    hash = { 0 => "Student", 1 => "Teacher", 2 => "Admin" }
+    data = {
+      id: current_user.id,
+      email: current_user.email,
+      role: hash[current_user.role1],
+    }
+
+    if user_admin or user_teacher
+      hash = { 0 => "Student", 1 => "Teacher", 2 => "Admin" }
+      data = {
+        id: current_user.id,
+        email: current_user.email,
+        role: hash[current_user.role1],
+        notices: gen_notices(current_user),
+      }
+    end
+
+    return data
+  end
+end
